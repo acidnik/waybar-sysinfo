a2ba462bb965e1c3d62ee5ba8ab7ed9f4c2225ef
Bart Nagel <bart@tremby.net>
2026-02-17 16:33:10 -0800

    Add nix files

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Waybar sysinfo plugin";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in {
+      packages = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./default.nix { };
+      });
+      devShells = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./shell.nix { };
+      });
+    };
+}
